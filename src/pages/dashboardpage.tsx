d2d3eb5a6b6343dedc4de965ae23dfe97ee29b3d
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-20 22:51:52 +0530

    Updated code files: dashboardpage.tsx

--- src/pages/dashboardpage.tsx
+++ src/pages/dashboardpage.tsx
@@ -18,7 +18,7 @@ const DashboardPage: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900">
+    <div className="flex flex-col min-h-screen bg-background"> {/* Updated to use bg-background for theme compliance */}
       <AppHeader />
       <main className="flex-1 flex flex-col items-center justify-center p-4 sm:p-6 lg:p-8 text-center">
         <Card className="w-full max-w-md shadow-xl">
@@ -40,9 +40,7 @@ const DashboardPage: React.FC = () => {
               Log Out
             </Button>
             {/* You could add links to other parts of the application if needed, e.g.: */}
-            {/* <p className="mt-4 text-sm">
-              <Link to="/settings" className="text-primary hover:underline">Account Settings</Link>
-            </p> */}
+            {/* <p className="mt-4 text-sm">\n              <Link to=\"/settings\" className=\"text-primary hover:underline\">Account Settings</Link>\n            </p> */}
           </CardContent>
         </Card>
       </main>
